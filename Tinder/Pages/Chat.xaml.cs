545cf1b0b1e049003544f472b609c090137f9b9e
mizworski <m.izworski7@gmail.com>
2017-08-28 19:10:52 +0200

    Removed test code

--- Tinder/Pages/Chat.xaml.cs
+++ Tinder/Pages/Chat.xaml.cs
@@ -216,62 +216,63 @@ namespace Tinder.Pages
             }
 
             LoadMessages((int) pairUserId, pairFirstName);
-            ReceiveMessages();
+//            ReceiveMessages();
         }
 
-        private void ReceiveMessages()
-        {
-
-            ISerializer aSerializer = new DataContractJsonStringSerializer();
-
-            // Create broker.
-            IDuplexBrokerFactory aBrokerFactory = new DuplexBrokerFactory();
-            IDuplexBroker aBroker = aBrokerFactory.CreateBroker();
-
-            // Communicate using WebSockets.
-            IMessagingSystemFactory aMessaging = new WebSocketMessagingSystemFactory();
-            IDuplexInputChannel anInputChannel =
-                aMessaging.CreateDuplexInputChannel("ws://127.0.0.1:8843/CpuUsage/");
-
-            anInputChannel.ResponseReceiverConnected += (x, y) =>
-            {
-                Console.WriteLine("Connected client: " + y.ResponseReceiverId);
-            };
-            anInputChannel.ResponseReceiverDisconnected += (x, y) =>
-            {
-                Console.WriteLine("Disconnected client: " + y.ResponseReceiverId);
-            };
-
-            // Attach input channel and start listeing.
-            aBroker.AttachDuplexInputChannel(anInputChannel);
-
-            // Start working thread monitoring the CPU usage.
-            bool aStopWorkingThreadFlag = false;
-            Thread aWorkingThread = new Thread(() =>
-            {
-                PerformanceCounter aCpuCounter =
-                    new PerformanceCounter("Processor", "% Processor Time", "_Total");
-
-                while (!aStopWorkingThreadFlag)
-                {
-                    CpuUpdateMessage aMessage = new CpuUpdateMessage();
-                    aMessage.Usage = aCpuCounter.NextValue();
-
-                    //Console.WriteLine(aMessage.Usage);
-
-                    // Serialize the message.
-                    object aSerializedMessage =
-                        aSerializer.Serialize<CpuUpdateMessage>(aMessage);
-
-                    // Notify subscribers via the broker.
-                    // Note: The broker will forward the message to subscribed clients.
-                    aBroker.SendMessage("MyCpuUpdate", aSerializedMessage);
-
-                    Thread.Sleep(500);
-                }
-            });
-            aWorkingThread.Start();
-        }
+//        private void ReceiveMessages()
+//        {
+//
+//            ISerializer aSerializer = new DataContractJsonStringSerializer();
+//
+//            // Create broker.
+//            IDuplexBrokerFactory aBrokerFactory = new DuplexBrokerFactory();
+//            IDuplexBroker aBroker = aBrokerFactory.CreateBroker();
+//
+//            // Communicate using WebSockets.
+//            IMessagingSystemFactory aMessaging = new WebSocketMessagingSystemFactory();
+//            IDuplexInputChannel anInputChannel =
+//                aMessaging.CreateDuplexInputChannel("ws://127.0.0.1:8843/CpuUsage/");
+//
+//            anInputChannel.ResponseReceiverConnected += (x, y) =>
+//            {
+//                Console.WriteLine("Connected client: " + y.ResponseReceiverId);
+//            };
+//            anInputChannel.ResponseReceiverDisconnected += (x, y) =>
+//            {
+//                Console.WriteLine("Disconnected client: " + y.ResponseReceiverId);
+//            };
+//
+//            // Attach input channel and start listeing.
+//            aBroker.AttachDuplexInputChannel(anInputChannel);
+//
+//            // Start working thread monitoring the CPU usage.
+//            bool aStopWorkingThreadFlag = false;
+//            Thread aWorkingThread = new Thread(() =>
+//            {
+//                PerformanceCounter aCpuCounter =
+//                    new PerformanceCounter("Processor", "% Processor Time", "_Total");
+//
+//                while (!aStopWorkingThreadFlag)
+//                {
+//                    CpuUpdateMessage aMessage = new CpuUpdateMessage();
+//                    aMessage.Usage = aCpuCounter.NextValue();
+//
+//                    //Console.WriteLine(aMessage.Usage);
+//
+//                    // Serialize the message.
+//                    object aSerializedMessage =
+//                        aSerializer.Serialize<CpuUpdateMessage>(aMessage);
+//
+//                    // Notify subscribers via the broker.
+//                    // Note: The broker will forward the message to subscribed clients.
+//                    aBroker.SendMessage("MyCpuUpdate", aSerializedMessage);
+//                    aBroker.
+//
+//                    Thread.Sleep(500);
+//                }
+//            });
+//            aWorkingThread.Start();
+//        }
 
         private void OnKeyDownHandler(object sender, KeyEventArgs e)
         {
